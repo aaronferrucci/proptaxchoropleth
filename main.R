15afd7ac346b207d48336e404004f5813e948a09
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-07-05 17:09:58 -0700

    Using custom colors for ggvis.

--- main.R
+++ main.R
@@ -18,14 +18,6 @@ library(maptools)
 library(rvest)
 library(ggplot2)
 
-get_apn_data <- function(apn) {
-  html <- get_apn_html(apn)
-  tax <- get_tax(html)
-  addr <- get_address(html)
-  type <- get_type(html)
-  return(list(tax=tax, addr=addr, type=type))
-}
-
 get_apn_html <- function(apn) {
   prefix <- "http://sccounty01.co.santa-cruz.ca.us/ASR/ParcelList/linkHREF"
   # The website suggests this "outside" value...
@@ -36,6 +28,31 @@ get_apn_html <- function(apn) {
 
   html <- read_html(url)
   return(html)
+}
+
+get_apn_data <- function(html) {
+  tax <- get_tax(html)
+  addr <- get_address(html)
+  type <- get_type(html)
+  exemption <- get_exemption(html)
+  return(list(tax=tax, addr=addr, type=type, exemption=exemption))
+}
+
+get_exemption <- function(html) {
+  nodes <- html_nodes(html, "body center table tr td table tr:nth-child(6) td div:nth-child(3) div div.plmTbody_AOM div")
+  exemption <- 0
+  if (length(nodes) >= 6) {
+    exStr <- html_text(nodes[6])
+    exemption <- dollarsToNumeric(exStr) 
+  }
+  return(exemption)
+}
+
+dollarsToNumeric <- function(x) {
+  # Drop '$', ','
+  p1 <- sub('$', '', x, fixed=TRUE)
+  p2 <- sub(',', '', p1, fixed=TRUE)
+  return(as.numeric(p2))
 }
 
 # return the total yearly tax as a numeric.
@@ -66,9 +83,15 @@ trim_info <- function (x) {
   return(x)
 }
 
+trim_address <- function(addr) {
+  addr <- gsub(", SANTA CRUZ,.*$", "", addr)
+}
+
 # return the parcel address as a character string.
 get_address <- function(html) {
-  return(get_parcel_info(html, 2))
+  addr <- get_parcel_info(html, 2)
+  addr <- trim_address(addr) 
+  return(addr)
 }
 
 get_type <- function(html) {
@@ -104,6 +127,7 @@ if (file.exists("data/final.plot.rda")) {
     apnnodash = sm$apnnodash,
     id = sm$objectid,
     tax = numeric(len),
+    exemption = numeric(len),
     addr = character(len),
     type = character(len)
   )
@@ -111,16 +135,33 @@ if (file.exists("data/final.plot.rda")) {
   # hack - addr shouldn't be factor
   parcel_data$addr <- as.character(parcel_data$addr)
   parcel_data$type <- as.character(parcel_data$type)
+ 
+  # Issue: loading saved parcel_html crashes R studio.
+  if (FALSE && file.exists("data/parcel_html.rda")) {
+    print("using cached html parcel data from data/parcel_html.rda")
+    load("data/parcel_html.rda")
+  } else {
+    parcel_html <- vector("list", len)
+    for (i in 1:len) {
+      apn <- parcel_data[i,]$apnnodash
+      print(paste("scraping data", "(", i, "of", len, ") for parcel", apn))
+      parcel_html[[i]] <- get_apn_html(apn)
+    }
+    save(parcel_html, file="data/parcel_html.rda")
+  }
 
   # Populate the df with parcel data
   for (i in 1:len) {
-    apn <- parcel_data[i,]$apnnodash
-    print(paste("scraping data", "(", i, "of", len, ") for parcel", apn))
-    apn_data <- get_apn_data(parcel_data[i,]$apnnodash)
+    html <- parcel_html[[i]]
+    apn_data <- get_apn_data(html)
     parcel_data[i,]$tax <- apn_data$tax
     parcel_data[i,]$addr <- apn_data$addr
     parcel_data[i,]$type <- apn_data$type
+    parcel_data[i,]$exemption <- apn_data$exemption
   }
+
+  # some derived parcel data
+  parcel_data$homeowner <- parcel_data$exemption == 7000
 
   # test that sm and parcel_data have the same APNNODASH
   all(sm$objectid == parcel_data$id)
@@ -144,5 +185,50 @@ p <- ggplot() +
   scale_fill_gradient(low = "white", high = "red")
 print(p)
 
+# ggvis experiment
+# have a look at https://github.com/vega/vega; maybe this
+# can export an svg file with tooltips.
+library(ggvis)
+get_row_by_group <- function(group) {
+  rows <- final.plot[final.plot$group == group, ]
+  row <- rows[1, ]
 
+  row
+}
+
+tt <- function(x) {
+  row <- get_row_by_group(x$group)
+  paste0(row$apnnodash, "<br/>", row$addr, "<br/>", "tax: $", row$tax, "<br/>",
+  "homeowner exemption: ", row$homeowner, "<br/>", row$type, "<br/>")
+}
+
+# make a palette from white, through yellow, to red.
+library(RColorBrewer)
+ramp <- colorRampPalette(
+  c("white", brewer.pal(n=9, name="YlOrRd")),
+  space="Lab"
+)
+# color for property tax
+final.plot$taxColor <- as.character(
+  cut(final.plot$tax, 20, include.lowest=TRUE, labels=ramp(20))
+)
+
+p2 <- final.plot %>%
+  ggvis(~long, ~lat) %>%
+  group_by(group, id) %>%
+  layer_paths(fill:=~taxColor, strokeOpacity := 0.5, strokeWidth := 0.25) %>%
+  add_tooltip(tt, "hover") %>%
+  set_options(width=100, height=600, keep_aspect=T)
+
+# homeowner's exemption
+final.plot$homeownerColor <- as.character(
+  cut(0 + final.plot$homeowner, 2, include.lowest=TRUE, labels=ramp(2))
+)
+p3 <- final.plot %>%
+  ggvis(~long, ~lat) %>%
+  group_by(group, id) %>%
+  layer_paths(fill:=~homeownerColor) %>%
+  add_tooltip(tt, "hover") %>%
+  scale_numeric("fill", range=c("#bfd3e6", "#8c6bb1" ,"#4d004b")) %>%
+  set_options(width=100, height=600, keep_aspect=T)
 
